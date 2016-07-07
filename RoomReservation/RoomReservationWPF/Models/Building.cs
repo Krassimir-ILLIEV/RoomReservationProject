a4eb6ebfbfee0e527de940052780698ba916635d
Pavel Apostolov <pavel.apostolov@yahoo.com>
2016-07-08 01:45:21 +0300

    Class Building not completed

--- RoomReservation/RoomReservationWPF/Models/Building.cs
+++ RoomReservation/RoomReservationWPF/Models/Building.cs
@@ -8,6 +8,60 @@ namespace RoomReservation.Models
 {
     class Building
     {
+        private int buildingID;
+        private enumBuildingTypes buildingType;
+        private enumBuildingLocation buildingLocation;
+        private int capacity;
+        private Coordinates coordianates;
+
+        public enumBuildingTypes BuildingType
+        {
+            get
+            {
+                return this.buildingType;
+            }
+
+            set
+            {
+                this.buildingType = value;
+            }
+        }
+
+        public enumBuildingLocation BuildingLocation
+        {
+            get
+            {
+                return this.buildingLocation;
+            }
+
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("Location must be set!");
+                }
+
+                this.buildingLocation = value;
+            }
+        }
+
+        public int Capacity
+        {
+            get
+            {
+                return capacity;
+            }
+        }
+
+        /*public Building(enumBuildingTypes buildingType,
+        enumBuildingLocation buildingLocation,
+        int capacity,
+        Coordinates coordinates)
+        {
+            this.buildingType = BuildingType;
+            this.buildingLocation = BuildingLocation;
+            this.capacity = Capacity;
+            */
 
     }
 }
