1431d2df5952835cb82b4ae57114a2943d4f929f
Pavel Apostolov <pavel.apostolov@yahoo.com>
2016-07-08 01:36:23 +0300

    Class Building, Class Coordinates, enum BuildingTypes, enum BuildingLocation
    not completed

--- RoomReservation/RoomReservationWPF/Models/Coordinates.cs
+++ RoomReservation/RoomReservationWPF/Models/Coordinates.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RoomReservation.Models
+﻿namespace RoomReservation.Models
 {
-    class Coordinates
+    public struct Coordinates
     {
+        private int x;
+        private int y;
+        //private static readonly int[] O = { 0, 0 };
+
+        public Coordinates(int xCoordinage, int yCoordinate)
+            : this()
+        {
+            this.X = xCoordinage;
+            this.Y = yCoordinate;
+        }
+
+        public int X
+        {
+            get { return this.x; }
+            set
+            {
+                this.x = value;
+            }
+        }
+
+        public int Y
+        {
+            get { return this.y; }
+            set
+            {
+
+                this.y = value;
+            }
+        }
     }
 }
