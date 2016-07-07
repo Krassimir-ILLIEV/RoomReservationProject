38b3550d36676cbe5de7c12a4cadf80535c162b5
didox2 <didox222@gmail.com>
2016-07-07 19:41:25 +0300

    Added some content in MediaDevice class

--- RoomReservation/RoomReservationWPF/Models/MultimediaDevice.cs
+++ RoomReservation/RoomReservationWPF/Models/MultimediaDevice.cs
@@ -5,7 +5,67 @@
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
+
     class MultimediaDevice
     {
+        private string model;
+        private int brightness;
+        private int suportedScreenSize;
+
+        public MultimediaDevice()
+        {
+
+        }
+
+        public MultimediaDevice(string model, int brightness, int suportedScreen)
+        {
+            this.model = Model;
+            this.brightness = Brightness;
+            this.suportedScreenSize = SuportedScreenSize;
+        }
+
+        public string Model
+        {
+            get
+            {
+                return this.model;
+            }
+            set
+            {
+                this.model = value;
+            }
+        }
+
+        public int Brightness
+        {
+            get
+            {
+                return this.brightness;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Brightness must be greater than 0");
+                }
+                this.brightness = value;
+            }
+        }
+
+        public int SuportedScreenSize
+        {
+            get
+            {
+                return this.suportedScreenSize;
+            }
+            set
+            {
+                if (value < 40 || value > 500)
+                {
+                    throw new ArgumentException("Suported screen size should be between 40 and 500 inches");
+                }
+                this.suportedScreenSize = value;
+            }
+        }
     }
 }
